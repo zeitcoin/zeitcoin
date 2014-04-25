439f373c27daa2ac79aaebfec44293fd6c62793c
ray77 <chakotay@bk.ru>
2014-04-25 14:41:31 +0200

    Testnet mod POW end at block 120

--- src/main.h
+++ src/main.h
@@ -36,7 +36,12 @@ static const int64 MAX_MONEY = 90000000000 * COIN;
 static const int64 CIRCULATION_MONEY = MAX_MONEY;
 static const double TAX_PERCENTAGE = 0.01;
 static const int64 MAX_MINT_PROOF_OF_STAKE = 0.05 * COIN;    // 5% annual interest
+
+#ifdef ENABLE_TESTNET
+static const int CUTOFF_POW_BLOCK = 120;
+#else
 static const int CUTOFF_POW_BLOCK = 260000;
+#endif
 
 static const int64 MIN_TXOUT_AMOUNT = MIN_TX_FEE;
 
