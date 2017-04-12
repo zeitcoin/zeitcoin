f096696c1c52e61a45d7e43366ca0c2f88701d6d
Rent_a_Ray <domy@synthetic-records.com>
2017-04-12 11:25:48 +0200

    Modifier-Interval to 10 mins

--- src/kernel.h
+++ src/kernel.h
@@ -7,7 +7,7 @@
 #include "main.h"
 
 // MODIFIER_INTERVAL: time to elapse before new modifier is computed
-static const unsigned int MODIFIER_INTERVAL = 6 * 60 * 60;
+static const unsigned int MODIFIER_INTERVAL = 10 * 60; // 10 Minutes
 extern unsigned int nModifierInterval;
 
 // MODIFIER_INTERVAL_RATIO:
